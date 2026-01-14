8dcf59af5edec92f34ca7a1e9236b63666d76105
Matías Simón <mati.agustin.simon@gmail.com>
2026-01-14 17:06:49 -0300

    feat: Complete exercise 2.8 (names + phones)

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
-const Person = ({ name }) => <li>{name}</li>
+const Person = ({name, phone}) => <li>{name} {phone}</li>
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        {
+            name: 'Arto Hellas',
+            phone: '012345-6789'
+        }
     ])
     const [newName, setNewName] = useState('')
+    const [newPhone, setNewPhone] = useState('')
 
     const handleNameChange = event => {
         setNewName(event.target.value)
     }
+    const handlePhoneChange = event => {
+        setNewPhone(event.target.value)
+    }
+
     const addPerson = event => {
         event.preventDefault()
         if (persons.some(person => person.name.trim() === newName.trim())) {
@@ -18,26 +26,27 @@ const App = () => {
             return
         }
         const newPersonObject = {
-            name: newName.trim()
+            name: newName.trim(),
+            phone: newPhone.trim()
         }
         setPersons(persons.concat(newPersonObject))
         setNewName('')
+        setNewPhone('')
     }
 
     return (
         <div>
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange}/>
-                </div>
+                <div>name: <input value={newName} onChange={handleNameChange}/></div>
+                <div>number: <input value={newPhone} onChange={handlePhoneChange}/></div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             <ul>
-                {persons.map((person) => <Person key={person.name} name={person.name} />)}
+                {persons.map((person) => <Person key={person.name} name={person.name} phone={person.phone}/>)}
             </ul>
         </div>
     )
