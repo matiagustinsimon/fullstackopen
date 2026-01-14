ba8fb438a2b83ffa73a651214b2bf23d2b72b83f
Matías Simón <mati.agustin.simon@gmail.com>
2026-01-14 01:06:03 -0300

    feat: Complete exercise 2.5 (Separate module)

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,23 +1,4 @@
-const Header = ({title}) => <h1>{title}</h1>
-const Content = ({parts}) => parts.map(part => <Part key={part.id} name={part.name} exercise={part.exercises} />)
-
-const Part = ({ name, exercise }) => <p>{name} {exercise}</p>
-
-const Sum = ({parts}) => {
-    const sum = parts.reduce((acc, curr) => acc + curr.exercises, 0);
-    return (<p><strong>Total of {sum} exercises</strong></p>)
-}
-
-const Course = ({course}) => {
-    return (
-        <>
-            <Header title={course.name} />
-            <Content parts={course.parts} />
-            <Sum parts={course.parts} />
-        </>
-
-    )
-}
+import Course from './components/Course.jsx'
 
 const App = () => {
     const courses = [
