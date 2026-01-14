a6d9b622e675689edcae4abf2b95c6322f25f830
Matías Simón <mati.agustin.simon@gmail.com>
2026-01-14 16:21:08 -0300

    feat: Complete exercise 2.6 (The Phonebook names)

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,55 +1,40 @@
-import Course from './components/Course.jsx'
+import { useState } from 'react'
+
+const Person = ({ name }) => <li>{name}</li>
 
 const App = () => {
-    const courses = [
-        {
-            name: 'Half Stack application development',
-            id: 1,
-            parts: [
-                {
-                    name: 'Fundamentals of React',
-                    exercises: 10,
-                    id: 1
-                },
-                {
-                    name: 'Using props to pass data',
-                    exercises: 7,
-                    id: 2
-                },
-                {
-                    name: 'State of a component',
-                    exercises: 14,
-                    id: 3
-                },
-                {
-                    name: 'Redux',
-                    exercises: 11,
-                    id: 4
-                }
-            ]
-        },
-        {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-                {
-                    name: 'Routing',
-                    exercises: 3,
-                    id: 1
-                },
-                {
-                    name: 'Middlewares',
-                    exercises: 7,
-                    id: 2
-                }
-            ]
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+
+    const handleNameChange = event => {
+        setNewName(event.target.value)
+    }
+    const addPerson = event => {
+        event.preventDefault()
+        const newPersonObject = {
+            name: newName
         }
-    ]
+        setPersons(persons.concat(newPersonObject))
+        setNewName('')
+    }
 
     return (
         <div>
-            <h1>Web development curriculum</h1>
-            {courses.map(course => (<Course key={course.id} course={course} /> ))}
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map((person) => <Person key={person.name} name={person.name} />)}
+            </ul>
         </div>
     )
 }
