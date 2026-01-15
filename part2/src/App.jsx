63bf07f7266c285e79fe3b4c8e60b3ecc639f94a
Matías Simón <mati.agustin.simon@gmail.com>
2026-01-15 02:40:31 -0300

    feat: Complete exercise 2.11 (fetch Server Json)

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,12 +1,15 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const Filter = ({value, onChange}) => <div>filter shown with <input value={value} onChange={onChange}/></div>
+const Filter = ({value, onChange}) => (
+    <div>filter shown with <input value={value} onChange={onChange}/></div>
+)
 
-const PersonForm = ({onSubmit, newName, handleName, newPhone, handlePhone}) => {
+const PersonForm = ({onSubmit, newName, handleName, newNumber, handleNumber}) => {
     return (
         <form onSubmit={onSubmit}>
             <div>name: <input value={newName} onChange={handleName}/></div>
-            <div>number: <input value={newPhone} onChange={handlePhone}/></div>
+            <div>number: <input value={newNumber} onChange={handleNumber}/></div>
             <div>
                 <button type="submit">add</button>
             </div>
@@ -14,23 +17,33 @@ const PersonForm = ({onSubmit, newName, handleName, newPhone, handlePhone}) => {
     )
 }
 
-const Person = ({name, phone}) => <li>{name} {phone}</li>
+const Person = ({name, number}) => {
+    return (<li>{name} {number}</li>)
+}
 
-const Persons = ({personsToShow}) =>
-    <ul>
-        {personsToShow.map(person => <Person key={person.id} name={person.name} phone={person.phone}/>)}
-    </ul>
+const Persons = ({personsToShow}) => {
+    return (
+        <ul>
+            {personsToShow.map(person =>
+                <Person key={person.id} name={person.name} number={person.number}/>
+            )}
+        </ul>
+    )
+}
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', phone: '040-123456', id: 1},
-        {name: 'Ada Lovelace', phone: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', phone: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
-    const [newPhone, setNewPhone] = useState('')
+    const [newNumber, setNewNumber] = useState('')
     const [search, setSearch] = useState('')
+
+    const handleFetch = () => {
+        axios.get('http://localhost:3001/persons').then((response) => {
+            setPersons(response.data)
+        })
+    }
+
+    useEffect(handleFetch, [])
 
     const personsToShow = search === ''
         ? persons
@@ -39,7 +52,7 @@ const App = () => {
         )
 
     const handleNameChange = event => setNewName(event.target.value)
-    const handlePhoneChange = event => setNewPhone(event.target.value)
+    const handleNumberChange = event => setNewNumber(event.target.value)
     const handleSearchChange = event => setSearch(event.target.value)
 
     const addPerson = event => {
@@ -48,14 +61,16 @@ const App = () => {
             alert(`${newName} is already added to phonebook`)
             return
         }
+
         const newPersonObject = {
             name: newName.trim(),
-            phone: newPhone.trim(),
-            id: persons.length + 1
+            number: newNumber.trim(),
+            id: String(persons.length + 1)
         }
+
         setPersons(persons.concat(newPersonObject))
         setNewName('')
-        setNewPhone('')
+        setNewNumber('')
     }
 
     return (
@@ -70,12 +85,11 @@ const App = () => {
                 onSubmit={addPerson}
                 newName={newName}
                 handleName={handleNameChange}
-                newPhone={newPhone}
-                handlePhone={handlePhoneChange}
+                newNumber={newNumber}
+                handleNumber={handleNumberChange}
             />
 
             <h3>Numbers</h3>
-
             <Persons personsToShow={personsToShow}/>
         </div>
     )
