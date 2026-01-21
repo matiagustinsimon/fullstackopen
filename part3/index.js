f1517ea192b8ce95c94780aa69f6630eb60ba6c4
Matías Simón <mati.agustin.simon@gmail.com>
2026-01-21 17:53:35 -0300

    feat: Complete exercise 3.1 (initial phonebook backend)

---
+++ part3/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let personsJson = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+
+// app.get('/api/notes/:id', (request, response) => {
+//     const id = request.params.id
+//     const note = notes.find(note => note.id === id)
+//     if (note) {
+//         response.json(note)
+//     } else {
+//         response.status(404).end()
+//     }
+// })
+
+app.get('/api/persons', (request, response) => {
+    response.json(personsJson)
+})
+
+// app.post('/api/notes', (request, response) => {
+//     const note = request.body
+//     console.log(note)
+//     response.json(note)
+// })
+
+// app.delete('/api/notes/:id', (request, response) => {
+//     const id = request.params.id
+//     notes = notes.filter(note => note.id !== id)
+//
+//     response.status(204).end()
+// })
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
