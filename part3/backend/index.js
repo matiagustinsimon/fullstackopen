a3ba105a3746dd3b0967b59a6980a44fbea0300b
Matías Simón <mati.agustin.simon@gmail.com>
2026-02-07 03:21:36 -0300

    feat: Complete exercise 3.15+3.16 (deleting + errorHandler)

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -20,60 +20,76 @@ app.use(morgan((tokens, req, res) => {
     return arrayFormat.join(' ')
 }))
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then((persons) => {
         return response.json(persons)
-    })
+    }).catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.countDocuments({}).then((count) => {
         const fecha = Date()
         response.send(`
         <p>Phonebook has info for ${count} people</p>
         <p>${fecha}</p>`)
-    })
+    }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
         } else {
             response.status(404).end()
         }
-    })
+    }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const newPerson = request.body
     if (!newPerson.name) {
-        return response.status(400).json({ error: 'no name sent' })
+        return response.status(400).json({error: 'no name sent'})
     }
     if (!newPerson.number) {
-        return response.status(400).json({ error: 'no number sent' })
+        return response.status(400).json({error: 'no number sent'})
     }
-    Person.findOne({ name: newPerson.name }).then((db_person) => {
-        if (db_person) {return response.status(400).json({ error: 'name must be unique' })}
+    Person.findOne({name: newPerson.name}).then((db_person) => {
+        if (db_person) {
+            return response.status(400).json({error: 'name must be unique'})
+        }
         const person = new Person({
             name: newPerson.name,
             number: newPerson.number,
         })
-
         person.save().then((savedPerson) => {
             response.json(savedPerson)
         })
-    })
+    }).catch(error => next(error))
 })
 
-//Useful later
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = request.params.id
-//     console.log(`Persona borrada: `, personsJSON.find(p => p.id === id));
-//     personsJSON = personsJSON.filter(p => p.id !== id)
-//     personsJSON.map(person => console.log(person))
-//     response.status(204).end()
-// })
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
