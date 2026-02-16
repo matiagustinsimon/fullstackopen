1aaf376900a5c2f564e121017fe3fe77a23abb85
Matías Simón <mati.agustin.simon@gmail.com>
2026-02-16 04:04:51 -0300

    feat: Complete exercise 4.12* (testing missing title or url handler)

--- part4/tests/HTTP.test.js
+++ part4/tests/HTTP.test.js
@@ -9,6 +9,13 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
+const newBlog = {
+  title: 'Second class tests',
+  author: 'Robert C. Martin',
+  url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+  likes: 100
+}
+
 beforeEach( async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(initialBlogs)
@@ -24,29 +31,28 @@ describe('Testing HTTP methods', () => {
     response.body.forEach(blog => {assert.strictEqual('id' in blog, true)})
   })
   test('HTTP POST BLOGS', async () => {
-    const newBlog = {
-      title: 'Second class tests',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 100
-    }
     await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
     const dbArray = await helper.blogsInDb()
     const titles = dbArray.map(blog => blog.title)
     assert(titles.includes('Second class tests'))
     assert.strictEqual(initialBlogs.length + 1, dbArray.length)
   })
-  test('missing likes', async () => {
-    const newBlog = {
-      title: 'Second class tests',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll'
-    }
-    const response = await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+  test('Missing likes', async () => {
+    // eslint-disable-next-line no-unused-vars
+    const { likes, ...blogWithoutLikes } = newBlog
+    const response = await api.post('/api/blogs').send(blogWithoutLikes).expect(201).expect('Content-Type', /application\/json/)
     const responseBlog = await Blog.findById(response.body.id)
     assert(responseBlog.likes === 0)
     const dbArray = await helper.blogsInDb()
     assert.strictEqual(initialBlogs.length + 1, dbArray.length)
+  })
+  test('Missing title or url', async () => {
+    // eslint-disable-next-line no-unused-vars
+    const { title, ...blogWithoutTitle } = { ...newBlog }
+    // eslint-disable-next-line no-unused-vars
+    const { url, ...blogWithoutUrl } = newBlog
+    await api.post('/api/blogs').send(blogWithoutTitle).expect(400)
+    await api.post('/api/blogs').send(blogWithoutUrl).expect(400)
   })
 })
 
