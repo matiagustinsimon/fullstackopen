832cfdc66d0d0ed84aa7db40e470d23c80a8f1f3
Matías Simón <mati.agustin.simon@gmail.com>
2026-02-20 19:40:16 -0300

    feat: Complete exercise 4.18+4.19  (add token-based auth and secure POST blogs with JWT)

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -1,5 +1,4 @@
 // noinspection JSCheckFunctionSignatures
-// const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
 const User = require('../models/user')
