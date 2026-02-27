5fab4ec92367c2a7fd90733ac059fea36a79ef4d
Matías Simón <mati.agustin.simon@gmail.com>
2026-02-26 23:19:56 -0300

    feat: Complete exercise 5.14 (testing: render all when view cliked)

--- part5/frontend/src/components/Blog.test.jsx
+++ part5/frontend/src/components/Blog.test.jsx
@@ -1,29 +1,46 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders only title and author', () => {
-  const blog = {
-    title: 'Testing props',
-    author: 'T. Tester',
-    url: 'test.com',
-    likes: 0,
-    user: { name: 'Admin', username: 'adminuser' }
-  }
-  render( <Blog
-    blog={blog}
-    blogs={[]}
-    setBlogs={vi.fn()}
-    setNotification={vi.fn()}
-    user={{ username: 'testuser' }}
-  /> )
+const blog = {
+  title: 'Testing props',
+  author: 'T. Tester',
+  url: 'test.com',
+  likes: 0,
+  user: { name: 'Admin', username: 'adminuser' }
+}
 
-  const elementTitle = screen.getByText(/Testing props T. Tester/)
-  expect(elementTitle).toBeDefined()
-  expect(elementTitle).toBeVisible()
-  const elementUrl = screen.getByText(/test.com/)
-  expect(elementUrl).not.toBeVisible()
-  const elementLikes = screen.getByText(/0/)
-  expect(elementLikes).not.toBeVisible()
-  const elementUser = screen.getByText(/Admin/)
-  expect(elementUser).not.toBeVisible()
+describe('Blog', () => {
+  beforeEach(() => {
+    render( <Blog
+      blog={blog}
+      blogs={[]}
+      setBlogs={vi.fn()}
+      setNotification={vi.fn()}
+      user={{ username: 'testuser' }}
+    /> )
+  })
+  test('renders only title and author', () => {
+    const elementTitle = screen.getByText(/Testing props T. Tester/)
+    expect(elementTitle).toBeDefined()
+    expect(elementTitle).toBeVisible()
+    const elementUrl = screen.getByText(/test.com/)
+    expect(elementUrl).not.toBeVisible()
+    const elementLikes = screen.getByText(/0/)
+    expect(elementLikes).not.toBeVisible()
+    const elementUser = screen.getByText(/Admin/)
+    expect(elementUser).not.toBeVisible()
+  })
+  test('after clicking the button, url, likes and users are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const elementUrl = screen.getByText(/test.com/)
+    expect(elementUrl).toBeVisible()
+    const elementLikes = screen.getByText(/likes 0/)
+    expect(elementLikes).toBeVisible()
+    const elementUser = screen.getByText(/Admin/)
+    expect(elementUser).toBeVisible()
+  })
 })
