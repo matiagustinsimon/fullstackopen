c88bf2f91ae7594f98398896c4eaa1d50af7189f
Matías Simón <mati.agustin.simon@gmail.com>
2026-03-01 05:07:35 -0300

    feat: Complete exercise 5.19 (testing: create blog works)

--- part5/tests/tests/blog.spec.js
+++ part5/tests/tests/blog.spec.js
@@ -2,6 +2,14 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 const user = {name: 'Matti Luukkainen', username: 'mluukkai', password: 'salainen'}
 
+const blog = {title: 'Luukkainen jornal', author: 'Matti Luukkainen', url: 'api.com'}
+
+const sendLogin = async (page, username, password) => {
+  await page.getByLabel('username').fill(username)
+  await page.getByLabel('password').fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     await request.post('/api/testing/reset')
@@ -19,18 +27,33 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByLabel('username').fill(user.username)
-      await page.getByLabel('password').fill(user.password)
-      await page.getByRole('button', { name: 'login' }).click()
+      await sendLogin(page, user.username, user.password)
       await expect(page.getByText(`${user.name} logged in`)).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByLabel('username').fill(user.username)
-      await page.getByLabel('password').fill('wrong-wrong')
-      await page.getByRole('button', { name: 'login' }).click()
+      await sendLogin(page, user.username, 'wrongPassword')
       await expect(page.getByText(`${user.name} logged in`)).not.toBeVisible()
       await expect(page.getByText('wrong credentials')).toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await sendLogin(page, user.username, user.password)
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'Create Blog' }).click()
+      await page.getByLabel('title').fill(blog.title)
+      await page.getByLabel('author').fill(blog.author)
+      await page.getByLabel('url').fill(blog.url)
+      await page.getByRole('button', { name: 'Create' }).click()
+      await expect(page.getByText(`a new blog ${blog.title} by ${blog.author} added`)).toBeVisible()
+      const blogContainer = page.locator('.blog-container').filter({ hasText: blog.title })
+      await expect(blogContainer).toBeVisible()
+      await expect(blogContainer).toContainText(blog.title)
+      await expect(blogContainer).toContainText(blog.author)
     })
   })
 })
