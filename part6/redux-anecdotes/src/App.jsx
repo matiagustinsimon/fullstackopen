3d252f91563b6ada027e29cd1633a0bf2fa5ccde
Matías Simón <mati.agustin.simon@gmail.com>
2026-03-07 02:31:03 -0300

    feat: Complete exercise 6.12 (add notificationReducer and display Notification component)

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,13 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
+import Notification from './components/Notification.jsx'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification />
       <Filter/>
       <AnecdoteList />
       <AnecdoteForm />
