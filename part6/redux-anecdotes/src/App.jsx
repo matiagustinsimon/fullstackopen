8221c746157df7fd12b5133bbebd9313d163ece8
Matías Simón <mati.agustin.simon@gmail.com>
2026-03-08 05:22:58 -0300

    feat: Complete exercise 6.14 (fetching anecdotes with Fetch API)

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,8 +2,16 @@ import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification.jsx'
+import {setAnecdotes} from './reducers/anecdoteReducer.js'
+import {useDispatch} from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import {useEffect} from 'react'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
